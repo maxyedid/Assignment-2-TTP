f15901061d5eb164a4afb113f97fbd4755d71188
Max Yedid <maxyedid1@gmail.com>
2022-01-05 14:33:10 -0500

    Added myEach method

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 function myEach(arr, callback) {
-    var t;
-    for (let i = 0; i < arr.length; i++) {
-        t = arr[i]
-        callback(t)
+    if (arr > 0 && typeof callback === 'function') {
+        var t;
+        for (let i = 0; i < arr.length; i++) {
+            t = arr[i]
+            callback(t)
+        }
     }
 }
 let arr1 = [1, 2, 3];
 
 myEach(arr1, function(element) {
-    element = 5;
     console.log(element)
 });
-console.log(arr1);
