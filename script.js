b26fe3d7ac55a5f5f6df23adc66711e8b0323a3c
Max Yedid <maxyedid1@gmail.com>
2022-01-05 14:32:11 -0500

    Made the myeach method

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function myEach(arr, callback) {
+    var t;
+    for (let i = 0; i < arr.length; i++) {
+        t = arr[i]
+        callback(t)
+    }
+}
+let arr1 = [1, 2, 3];
+
+myEach(arr1, function(element) {
+    element = 5;
+    console.log(element)
+});
+console.log(arr1);
